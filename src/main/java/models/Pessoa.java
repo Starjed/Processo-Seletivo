10d1129a2331031efc66172bdb1cccb34f14011f
Gabriel Albuquerque <biel123albuquerque@hotmail.com>
2025-03-28 20:19:56 -0400

    Adiciona pessoa

---
+++ src/main/java/models/Pessoa.java
@@ -0,0 +1,28 @@
+package models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "pessoa")
+class Pessoa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long pesId;
+
+    @Column(nullable = false, length = 200)
+    private String pesNome;
+
+    @Column(nullable = false)
+    private LocalDate pesDataNascimento;
+
+    @Column(length = 9)
+    private String pesSexo;
+
+    @Column(length = 200)
+    private String pesMae;
+
+    @Column(length = 200)
+    private String pesPai;
+}
